c29fd0b1ac35ded48bea674634eec98b9211e3ab
tanyalai <tanyalai@umich.edu>
2019-08-09 11:20:00 +0100

    Add base sentiment analysis

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Message } from '../models/message';
-
+import * as Sentiment from 'sentiment';
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
+  sentiment = new Sentiment();
+
   constructor(private firestore: AngularFirestore) {
    }
-   
+
   getMessages() {
     return this.firestore.collection('messages').snapshotChanges();
   }
@@ -17,4 +19,12 @@ export class MessageService {
   createMessage(message: Message) {
     return this.firestore.collection('messages').add({...message});
   }
+
+  computeSentimentScore(message: Message){
+    const score: number = this.sentiment.analyze(message.message);
+    message.score = score;
+    return score;
+  }
+
+  //extra time delete and update messages
 }
